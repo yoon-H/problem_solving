cf74f1094fbd6d7bf5a0931bcfd17187fed67816
yoon-H <71068767+yoon-H@users.noreply.github.com>
2025-01-31 09:26:36 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 15.66 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -0,0 +1,28 @@
+function solution(r1, r2) {
+    var answer = 0;
+    
+    const r1Pow = r1*r1;
+    const r2Pow = r2*r2;
+    
+    
+    for(let i=1; i<=r2; i++) {
+        
+        const xPow = i*i;
+        
+        const y2 = Math.floor(Math.sqrt(r2Pow - xPow, 2));
+        
+        let y1;
+        
+        y1 = Math.ceil(Math.sqrt(r1Pow - xPow, 2));
+        
+        if(i > r1) y1 = 0;
+        
+        if(y2 < y1) continue;
+        
+        answer += y2 - y1 + 1;
+    }
+    
+    answer *= 4 ;
+    
+    return answer;
+}
